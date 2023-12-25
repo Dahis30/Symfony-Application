9b4c3bc91e3a6b9c3a4d4c827368f4c2dddee22d
Dahis30 <mohamednadir637@gmail.com>
2023-12-25 10:11:26 +0100

    category changes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +22,23 @@ class ArticleController extends AbstractController
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
             'articles'=> $articles ,
+        ]);
+    }
+    #[Route('/blog/new', name: 'blog_create')]
+    public function create(Request $request ,EntityManagerInterface $manager): Response
+    {    
+        if ($request->request->count()>0){
+            $article = new Article;
+            $article->setTitle($request->request->get('title'))
+                    ->setContent($request->request->get('content'))
+                    ->setImage($request->request->get('image'))
+                    ->setCreatedAt(new \DateTimeImmutable());
+                    $manager->persist($article);
+                    $manager->flush();
+                    return  $this->redirectToRoute('blog_show',['id' => $article->getId()]);
+        }
+        return $this->render('article/create_blog.html.twig', [
+            'controller_name' => 'ArticleController',
         ]);
     }
     #[Route('/blog/{id}', name: 'blog_show')]
